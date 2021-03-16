b3fa3badf2f2989b5e50277e98465767c28ea7fe
lmr87 <lmr87@DESKTOP-G9P7QE6>
2021-03-16 12:59:13 +0100

    Punto 1.2
    Creados los métodos get y set de la clase Casilla, con las excepciones
    adecuadas

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -2,4 +2,16 @@ package org.iesalandalus.programacion.cuatroenraya.modelo;
 
 public class Casilla {
 	private Ficha ficha = null;
+
+	public Ficha getFicha() {
+		return ficha;
+	}
+
+	public void setFicha(Ficha ficha) {
+		if (ficha == null) {
+			throw new NullPointerException("ERROR: No se puede poner una ficha nula.");
+		}
+		
+		this.ficha = ficha;
+	}
 }
