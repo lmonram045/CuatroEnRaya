bea124dc2bf0c5c4fb7d6e04667500257be926f6
lmr87 <lmr87@DESKTOP-G9P7QE6>
2021-03-17 10:07:33 +0100

    Puntos 3.2 y 3.3
    Creados los métodos get y set con la visibilidad adecuada y sus
    excepciones y el constructor con parámetros Jugador(nombre, ficha)

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
@@ -3,5 +3,32 @@ package org.iesalandalus.programacion.cuatroenraya.modelo;
 public class Jugador {
 	private String nombre;
 	private Ficha ficha;
+	public Jugador(String nombre, Ficha ficha) throws NullPointerException{
+		setNombre(nombre);
+		setFicha(ficha);
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public Ficha getColorFichas() {
+		return ficha;
+	}
+	private void setNombre(String nombre) {
+		if (nombre == null) {
+			throw new NullPointerException("ERROR: El nombre no puede ser nulo.");
+		}
+		
+		if (nombre.trim() == "") {
+			throw new IllegalArgumentException("ERROR: El nombre no puede estar vacío.");
+		}
+		this.nombre = nombre;
+	}
+	private void setFicha(Ficha ficha) {
+		if (ficha == null) {
+			throw new NullPointerException("ERROR: El color de las fichas no puede ser nulo.");
+		}
+		this.ficha = ficha;
+	}
 	
 }
