7020046bcf73577622d7646cf80557e66f36839b
luis <luis@debian.casa>
2021-03-24 23:34:48 +0100

    Punto 6.1
    Creación de la clase CuatroEnRaya y de una constante para indicar el
    número de jugadores, un array de objetos del tipo jugador y un atributo
    de la clase tablero.

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/CuatroEnRaya.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.cuatroenraya;
+
+import org.iesalandalus.programacion.cuatroenraya.modelo.Jugador;
+import org.iesalandalus.programacion.cuatroenraya.modelo.Tablero;
+
+public class CuatroEnRaya {
+	static final int NUMERO_JUGADORES = 2;
+	Jugador[] jugadores = new Jugador[NUMERO_JUGADORES];
+	Tablero tablero;
+}
