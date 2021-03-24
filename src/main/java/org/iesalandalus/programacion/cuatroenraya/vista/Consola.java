05ecf212809c140023beac7376c6d07a00b6b588
lmr87 <lmr87@DESKTOP-G9P7QE6>
2021-03-24 11:05:22 +0100

    Punto 5.2
    Creado el método leerNombre, que leera el nombre del jugador por
    teclado.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/vista/Consola.java
@@ -1,7 +1,19 @@
 package org.iesalandalus.programacion.cuatroenraya.vista;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
 	private Consola() {
 		
+	}
+	
+	public static String leerNombre() {
+		String nombre = new String();
+		do {
+			System.out.print("Introduzca el nombre de el jugador: ");
+			nombre = Entrada.cadena();
+		} while (nombre == null || nombre.trim() == "");
+				
+		return nombre;
 	}
 }
