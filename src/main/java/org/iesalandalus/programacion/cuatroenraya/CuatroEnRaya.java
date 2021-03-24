15a0798597cc9dba1ab3903df3a4b33fe5ff26fd
luis <luis@debian.casa>
2021-03-25 00:29:49 +0100

    Punto 6.2
    Creado el constructor con parámetros al que le pasamos dos instancias
    del tipo Jugador como parámetros representando a cada uno de los
    jugadores. Hace las comprobaciones y excepciones adecuadas.
    Crea el array de jugadores y lo inicializa y crea la instancia de la
    clase Tablero. Empezado el siguiente método tirar, hago commit para
    sincronizar con pc grande.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/CuatroEnRaya.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/CuatroEnRaya.java
@@ -7,4 +7,16 @@ public class CuatroEnRaya {
 	static final int NUMERO_JUGADORES = 2;
 	Jugador[] jugadores = new Jugador[NUMERO_JUGADORES];
 	Tablero tablero;
+	
+	public CuatroEnRaya(Jugador jugador1, Jugador jugador2) {
+		jugadores[0] = jugador1;
+		jugadores[1] = jugador2;
+		tablero = new Tablero();
+		//no se si tengo que hacer algo mas para comprobar según el enunciado
+	}
+	
+	private boolean tirar(Jugador jugador) {
+		
+	}
 }
+
